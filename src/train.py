7a0a5ec6124afd25ef3ec39f13e1432f0d164752
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-10-01 15:22:29 +0000

    avanzando

---
+++ src/train.py
@@ -0,0 +1,5 @@
+# train.py
+# Este script será usado para entrenar una Red Neuronal Convolucional (CNN) con el dataset MNIST
+# para reconocer dígitos manuscritos (0-9). Utilizará TensorFlow/Keras y guardará el modelo
+# en la carpeta 'models/' como modelo_digitos.h5. Basado en los apuntes de clase (Temario.pdf,
+# páginas 9-17).
