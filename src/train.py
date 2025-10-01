4ff17e7aa9370e90cf408d93a42420517640a7e9
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-10-01 15:31:23 +0000

    Implementada función crear_modelo() en src/train.py para definir la arquitectura de la CNN

--- src/train.py
+++ src/train.py
@@ -40,3 +40,16 @@ def cargar_datos():
     y_test = to_categorical(y_test, 10)
     
     return X_train, y_train, X_test, y_test
+
+def crear_modelo():
+    # Crear la arquitectura de la CNN
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, (5, 5), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(36, (5, 5), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    return model
